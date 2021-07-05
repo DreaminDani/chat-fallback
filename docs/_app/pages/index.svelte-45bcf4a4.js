00dc445d30f66198e2059cac967d3ef37bcf223c
Daniel Sandoval <daniel@desandoval.net>
2021-07-05 08:52:25 -0700

    no static adapter

---
+++ docs/_app/pages/index.svelte-45bcf4a4.js
@@ -0,0 +1,1 @@
+import"../chunks/preload-helper-9f12a5fd.js";import"../chunks/vendor-f1f712b3.js";export{R as default}from"../chunks/index-74d30246.js";
--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,7 @@
-import adapter from '@sveltejs/adapter-static';
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		target: '#svelte',
-		prerender: {
-			enabled: false
-		},
-		adapter: adapter({
-			pages: 'docs',
-			assets: 'docs',
-			fallback: null
-		})
 	}
 };
 
