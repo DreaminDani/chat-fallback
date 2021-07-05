eacbccfad30d12208f556bca6edcda5950783155
Daniel Sandoval <daniel@desandoval.net>
2021-07-05 09:04:32 -0700

    modify build without chat

--- docs/_app/pages/index.svelte-f70d920c.js
+++
@@ -1,1 +0,0 @@
-import"../chunks/preload-helper-9f12a5fd.js";import"../chunks/vendor-f1f712b3.js";export{R as default}from"../chunks/index-40127696.js";
--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,21 @@
+import adapter from '@sveltejs/adapter-static';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		target: '#svelte',
+		// Comment the paths config, if you want to run in dev mode.
+		// Remove if you're not using github pages (or don't need rel url)
+		paths: {
+			base: '/chat-fallback', // replace this with your url
+			assets: '/chat-fallback' // replace this with your url
+		},
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		})
 	}
 };
 
